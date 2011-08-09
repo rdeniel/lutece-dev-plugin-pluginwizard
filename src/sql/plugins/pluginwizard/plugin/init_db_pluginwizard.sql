6e0e6f432317353ce478bfe6f933eab7ccfad99f
tla-dev <tla-dev@users.noreply.github.com>
2011-08-09 15:28:02 +0000

    pom parent 2.17 -> 2.26
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/plugin-pluginwizard@33145 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/pluginwizard/plugin/init_db_pluginwizard.sql
+++ src/sql/plugins/pluginwizard/plugin/init_db_pluginwizard.sql
@@ -3,7 +3,7 @@
 -- Dumping data for table `pluginwizard_configuration_key`
 --
 INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (1,'plugin_version_number','3.0');
-INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (2,'pom_parent_version','2.17');
+INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (2,'pom_parent_version','2.26');
 INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (3,'pom_model_version','4.0.0');
 INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (4,'pom_dependencies_core_version','[2.2.4,3.0.0-SNAPSHOT)');
 
