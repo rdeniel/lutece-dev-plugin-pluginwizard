c4a7dd0283924cc7179f24b6bf554d8d01e8fcb8
tla-dev <tla-dev@users.noreply.github.com>
2011-08-12 09:53:18 +0000

    PLUGINWIZARD-38 pom_dependencies_core_version update
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/plugin-pluginwizard@33331 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/pluginwizard/plugin/init_db_pluginwizard.sql
+++ src/sql/plugins/pluginwizard/plugin/init_db_pluginwizard.sql
@@ -5,7 +5,7 @@
 INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (1,'plugin_version_number','3.0');
 INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (2,'pom_parent_version','2.26');
 INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (3,'pom_model_version','4.0.0');
-INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (4,'pom_dependencies_core_version','[2.2.4,3.0.0-SNAPSHOT)');
+INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (4,'pom_dependencies_core_version','[2.2.4,)');
 
 --
 -- Dumping data for table `pluginwizard_plugin_application`
