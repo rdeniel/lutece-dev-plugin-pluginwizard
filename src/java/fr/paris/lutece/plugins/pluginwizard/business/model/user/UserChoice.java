74da346d408414357dc29856bf07c0cd6d37575c
pierrelevy <pierrelevy@users.noreply.github.com>
2013-01-04 16:20:11 +0000

    PLUGINWIZARD-39 : Bootstrap integratrion for Lutece v4 - Work in progress
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/plugin-pluginwizard@46158 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/pluginwizard/business/model/user/UserChoice.java
+++ src/java/fr/paris/lutece/plugins/pluginwizard/business/model/user/UserChoice.java
@@ -35,14 +35,15 @@ package fr.paris.lutece.plugins.pluginwizard.business.model.user;
 
 import java.io.Serializable;
 
-
 /**
  *
- * The class will determine the choices of the user during the navigation process
+ * The class will determine the choices of the user during the navigation
+ * process
  *
  */
 public class UserChoice implements Serializable
 {
+
     private static final long serialVersionUID = 5783514241945024840L;
     private boolean _bBusinessClasses;
     private boolean _bJspBean;
@@ -56,200 +57,237 @@ public class UserChoice implements Serializable
     private boolean _bSpringContextXml;
     private boolean _bXpages;
 
+    public UserChoice()
+    {
+        _bBusinessClasses = true;
+        _bJspBean = true;
+        _bSqlFiles = true;
+        _bBackOfficeTemplate = true;
+        _bResourceFiles = true;
+        _bBackOfficeJsp = true;
+        _bPluginPropertiesFile = true;
+        _bPluginXmlDefinition = true;
+        _bMavenPomXml = true;
+        _bSpringContextXml = true;
+        _bXpages = true;
+    }
+
     /**
      * Returns boolean value whether to generate business classes
+     *
      * @return if true business classes are generated
      */
-    public boolean getBusinessClasses(  )
+    public boolean getBusinessClasses()
     {
         return _bBusinessClasses;
     }
 
     /**
      * Sets the business class choice
+     *
      * @param bBusinessClasses The boolean value to generate business class
      */
-    public void setBusinessClasses( boolean bBusinessClasses )
+    public void setBusinessClasses(boolean bBusinessClasses)
     {
         _bBusinessClasses = bBusinessClasses;
     }
 
     /**
      * Fetches the JspBean choice
+     *
      * @return if true jspbean are generated
      */
-    public boolean getJspBean(  )
+    public boolean getJspBean()
     {
         return _bJspBean;
     }
 
     /**
      * Sets the bJspBeanChoice
-     * @param bJspBeanChoice  The boolean value to generate business class
+     *
+     * @param bJspBeanChoice The boolean value to generate business class
      */
-    public void setJspBean( boolean bJspBeanChoice )
+    public void setJspBean(boolean bJspBeanChoice)
     {
         _bJspBean = bJspBeanChoice;
     }
 
     /**
      * Fetches the sql choice
+     *
      * @return if true sql files are generated
      */
-    public boolean getSqlFiles(  )
+    public boolean getSqlFiles()
     {
         return _bSqlFiles;
     }
 
     /**
      * Sets the sql files generation
+     *
      * @param bSqlFiles The boolean value to generate sql files
      */
-    public void setSqlFiles( boolean bSqlFiles )
+    public void setSqlFiles(boolean bSqlFiles)
     {
         _bSqlFiles = bSqlFiles;
     }
 
     /**
      * Fetches the back office templates choice
+     *
      * @return if true back office templates are generated
      */
-    public boolean getBackOfficeTemplate(  )
+    public boolean getBackOfficeTemplate()
     {
         return _bBackOfficeTemplate;
     }
 
     /**
-     * Sets the back office templates generation  choice
+     * Sets the back office templates generation choice
+     *
      * @param bBackOfficeTemplate The boolean value
      */
-    public void setBackOfficeTemplate( boolean bBackOfficeTemplate )
+    public void setBackOfficeTemplate(boolean bBackOfficeTemplate)
     {
         _bBackOfficeTemplate = bBackOfficeTemplate;
     }
 
     /**
      * Fetches resource files choice
+     *
      * @return if true resource files are generated
      */
-    public boolean getResourceFiles(  )
+    public boolean getResourceFiles()
     {
         return _bResourceFiles;
     }
 
     /**
-     * Sets the resource files generation  choice
+     * Sets the resource files generation choice
+     *
      * @param bResourceFiles The boolean value
      */
-    public void setResourceFiles( boolean bResourceFiles )
+    public void setResourceFiles(boolean bResourceFiles)
     {
         _bResourceFiles = bResourceFiles;
     }
 
     /**
-     * Fetches back office jsp  files choice
+     * Fetches back office jsp files choice
+     *
      * @return if true back office jsp are generated
      */
-    public boolean getBackOfficeJsp(  )
+    public boolean getBackOfficeJsp()
     {
         return _bBackOfficeJsp;
     }
 
     /**
      * Sets the back office jsp choice
+     *
      * @param bBackOfficeJsp The boolean value
      */
-    public void setBackOfficeJsp( boolean bBackOfficeJsp )
+    public void setBackOfficeJsp(boolean bBackOfficeJsp)
     {
         _bBackOfficeJsp = bBackOfficeJsp;
     }
 
     /**
-     *  Fetches plugin properties  files choice
+     * Fetches plugin properties files choice
+     *
      * @return if true plugin properties file are generated
      */
-    public boolean getPluginPropertiesFile(  )
+    public boolean getPluginPropertiesFile()
     {
         return _bPluginPropertiesFile;
     }
 
     /**
      * Sets plugin properties choice
+     *
      * @param bPluginPropertiesFile The boolean value
      */
-    public void setPluginPropertiesFile( boolean bPluginPropertiesFile )
+    public void setPluginPropertiesFile(boolean bPluginPropertiesFile)
     {
         _bPluginPropertiesFile = bPluginPropertiesFile;
     }
 
     /**
      * Fetches xml describing the plugin choice
+     *
      * @return if true the xml describing the plugin is generated
      */
-    public boolean getPluginXmlDefinition(  )
+    public boolean getPluginXmlDefinition()
     {
         return _bPluginXmlDefinition;
     }
 
     /**
      * Sets the xml plugin choice
+     *
      * @param bPluginXmlDefinition The boolean value
      */
-    public void setPluginXmlDefinition( boolean bPluginXmlDefinition )
+    public void setPluginXmlDefinition(boolean bPluginXmlDefinition)
     {
         _bPluginXmlDefinition = bPluginXmlDefinition;
     }
 
     /**
      * Fetches pom.xml choice
+     *
      * @return if true the pom.xml of the plugin is generated
      */
-    public boolean getMavenPomXml(  )
+    public boolean getMavenPomXml()
     {
         return _bMavenPomXml;
     }
 
     /**
      * Sets the maven pom choice
+     *
      * @param bMavenPomXml The boolean value
      */
-    public void setMavenPomXml( boolean bMavenPomXml )
+    public void setMavenPomXml(boolean bMavenPomXml)
     {
         _bMavenPomXml = bMavenPomXml;
     }
 
     /**
      * Fetches spring context file choice
+     *
      * @return if true the context file is generated
      */
-    public boolean getSpringContextXml(  )
+    public boolean getSpringContextXml()
     {
         return _bSpringContextXml;
     }
 
     /**
      * Sets the context file used by spring choice
+     *
      * @param bSpringContextXml The boolean value
      */
-    public void setSpringContextXml( boolean bSpringContextXml )
+    public void setSpringContextXml(boolean bSpringContextXml)
     {
         _bSpringContextXml = bSpringContextXml;
     }
 
     /**
      * Fetches Xpagese choice
+     *
      * @return if true the Xpages are generated
      */
-    public boolean getXpages(  )
+    public boolean getXpages()
     {
         return _bXpages;
     }
 
     /**
      * Sets the XPages choice
+     *
      * @param bXpages The boolean value
      */
-    public void setXpages( boolean bXpages )
+    public void setXpages(boolean bXpages)
     {
         _bXpages = bXpages;
     }
