63367904418cf89b10ce398bda3fda1e2d14f45b
Adrien Duchemin <adrien.duchemin@paris.fr>
2016-03-09 15:39:26 +0100

    Swagger file generation

---
+++ src/java/fr/paris/lutece/plugins/pluginwizard/service/generator/RestSwaggerGenerator.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2002-2016, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.pluginwizard.service.generator;
+
+import fr.paris.lutece.plugins.pluginwizard.business.model.BusinessClass;
+import fr.paris.lutece.plugins.pluginwizard.business.model.PluginModel;
+import fr.paris.lutece.plugins.pluginwizard.service.ModelService;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * The rest swagger file generator
+ *
+ */
+public class RestSwaggerGenerator extends AbstractGenerator
+{
+    private static final String PATH = "webapp/plugins/{plugin_name}/swagger/v1/";
+    private static final String CONSTANT_SWAGGER = "swagger.json";
+
+    /**
+     * {@inheritDoc }
+     * @param pm
+     */
+    @Override
+    public Map generate( PluginModel pm )
+    {
+        HashMap map = new HashMap(  );
+        
+        Collection<BusinessClass> listBusinessClasses = ModelService.getBusinessClassesByRest(pm);
+
+        if ( !listBusinessClasses.isEmpty(  ) )
+        {
+            String strPath = getFilePath( pm, PATH, CONSTANT_SWAGGER );
+            String strSourceCode = getPage( pm, listBusinessClasses );
+            map.put( strPath, strSourceCode );
+        }
+        return map;
+    }
+
+    /**
+    * Generates the Rest code
+    * @param pm The plugin model
+    * @param businessClasses the collection of business classes
+    * @return The code of the Rest generated
+    */
+    private String getPage( PluginModel pm, Collection <BusinessClass> businessClasses )
+    {
+        Map<String, Object> model = getModel( pm );
+        model.put( Markers.MARK_PLUGIN, pm );
+        model.put( Markers.MARK_BUSINESS_CLASSES, businessClasses );
+
+        return build( model );
+    }
+}
