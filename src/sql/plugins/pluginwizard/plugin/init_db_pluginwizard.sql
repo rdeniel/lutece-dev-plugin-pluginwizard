2102d11b59f5e97bb71375f2cb4e52d16444f1b6
seboo <25958061+seboo@users.noreply.github.com>
2017-08-11 14:29:11 +0200

    PLUGINWIZARD-115 insert all pom versions from database keys into pom generator template - add rest pom version

--- src/sql/plugins/pluginwizard/plugin/init_db_pluginwizard.sql
+++ src/sql/plugins/pluginwizard/plugin/init_db_pluginwizard.sql
@@ -3,8 +3,8 @@
 -- Dumping data for table `pluginwizard_configuration_key`
 --
 INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (1,'plugin_version_number','1.0.0');
-INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (2,'pom_parent_version','3.7');
+INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (2,'pom_parent_version','4.0.3');
 INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (3,'pom_model_version','4.0.0');
-INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (4,'pom_dependencies_core_version','[4.9.9,)');
-
+INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (4,'pom_dependencies_core_version','[6.0.0,)');
+INSERT INTO pluginwizard_configuration_key (id_key, key_description, key_value) VALUES (5,'pom_dependencies_rest_version','[3.0.0,)');
  
