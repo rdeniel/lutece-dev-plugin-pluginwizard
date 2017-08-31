rdeniel/lutece-dev-plugin-pluginwizard

commit 1c2ee9c87b9345719a5b84a0060ec6f12dae7fa4

PLUGINWIZARD-114 centralize all naming methods to the pluginModel bean

src/java/fr/paris/lutece/plugins/pluginwizard/service/generator/ResourcesGenerator.java
@@ -68,16 +68,8 @@ public class ResourcesGenerator extends AbstractGenerator
     {
         HashMap map = new HashMap(  );
         
-        String prefixFileName;
+        String prefixFileName = pm.getPluginNameForRessource();
         
-        if(Utils.MODULE.equals(pm.getType())){
-        	
-        	prefixFileName=pm.getPluginName(  ).split("-")[1].toLowerCase( );
-        
-        }else{
-        
-        	prefixFileName=pm.getPluginName(  ).toLowerCase( );
-        }
         for ( String strLanguage : _languages )
         {
             String strPath = getFilePath( pm, PATH,
@@ -120,14 +112,17 @@ public class ResourcesGenerator extends AbstractGenerator
      */
     private void generatePluginKeys( StringBuilder sb, PluginModel pm )
     {
-    	 sb.append( "# Plugin's keys\n" );
-    	 if(Utils.MODULE.equals(pm.getType( ))){
-    		 sb.append( "module.provider=" ).append( pm.getPluginProvider(  ) ).append( "\n" );
-    	     sb.append( "module.description=" ).append( pm.getPluginDescription(  ) ).append( "\n" );         	
-         }else{
-        	 sb.append( "plugin.provider=" ).append( pm.getPluginProvider(  ) ).append( "\n" );
-    	     sb.append( "plugin.description=" ).append( pm.getPluginDescription(  ) ).append( "\n" );
-         }
+    	sb.append( "# Plugin's keys\n" );
+    	if ( pm.isModule( ) )
+        {
+            sb.append( "module.provider=" ).append( pm.getPluginProvider(  ) ).append( "\n" );
+    	    sb.append( "module.description=" ).append( pm.getPluginDescription(  ) ).append( "\n" );         	
+        }
+        else
+        {
+            sb.append( "plugin.provider=" ).append( pm.getPluginProvider(  ) ).append( "\n" );
+    	    sb.append( "plugin.description=" ).append( pm.getPluginDescription(  ) ).append( "\n" );
+        }
        
         sb.append( "\n" );
     }

src/java/fr/paris/lutece/plugins/pluginwizard/business/model/PluginModel.java
@@ -42,6 +42,7 @@ import java.util.Locale;
 
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import org.codehaus.jackson.annotate.JsonIgnore;
 
 
 /**
@@ -80,6 +81,7 @@ public class PluginModel
     private List<Portlet> _listPluginPortlets;
     private List<BusinessClass> _listBusinessClasses;
     private Rest _rest;
+    private boolean _bIsModule;
 
     /**
      *
@@ -119,6 +121,99 @@ public class PluginModel
     {
         return _strPluginName;
     }
+    
+    /**
+     * Returns the Plugin or Module name formatted with dots as package
+     * @return The PluginName
+     */
+    @JsonIgnore
+    public String getPluginNameAsRadicalPackage(  )
+    {
+        if ( isModule( ) ) {
+            return _strPluginName.split("-")[0] + ".modules." + _strPluginName.split("-")[1]; 
+        } 
+        else
+        {
+            return _strPluginName ;
+        }
+
+    }
+    
+    /**
+     * Returns the Plugin or Module name formatted with slashes as Path
+     *
+     * @return The PluginName
+     */
+    @JsonIgnore
+    public String getPluginNameAsRadicalPath(  )
+    {
+        if ( isModule() ) 
+        {      
+        	return _strPluginName.split("-")[0]+"/modules/" + _strPluginName.split("-")[1];
+        }
+        else
+        {        	
+        	return _strPluginName ;
+        }
+    }
+        
+    /**
+     * Returns the Plugin or Module name for i18n keus 
+     * @return The PluginName
+     */
+    @JsonIgnore
+    public String getPluginNameAsPackage(  )
+    {
+        if ( isModule( ) ) 
+        {
+            return "module." + _strPluginName.replace("-", ".") ;        	
+        } 
+        else
+        {
+            return _strPluginName ;
+        }
+
+    }
+    
+        /**
+     * Returns the Plugin or Module name for Ressource 
+     * @return The PluginName
+     */
+    @JsonIgnore
+    public String getPluginNameForRessource(  )
+    {
+       if ( isModule( ) ) 
+       {
+            return getModuleName(  ).toLowerCase( );
+       } 
+       else 
+       {
+            return _strPluginName.toLowerCase( );
+       }
+
+    }
+      
+    
+    
+    /**
+     * Returns the Plugin or Module name 
+     * @return The PluginName
+     */
+    @JsonIgnore
+    public String getModuleName(  )
+    {
+        if ( isModule( ) ) 
+        {
+            return getPluginName( ).split("-")[1] ;
+        } 
+        else
+        {
+            return "";
+        }
+    }
+        
+    
+    
 
     /**
      * Sets the PluginName
@@ -449,6 +544,7 @@ public class PluginModel
         throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
     }
 
+    
     /**
      * Returns the Type (module or plugin)
      * @return The Type
@@ -460,10 +556,30 @@ public class PluginModel
 
     /**
      * Sets the Type
-     * @param bIsModule The Type
+     * @param  The Type
      */
     public void setType( String strType )
     {
     	_strType = strType;
     }
+    /**
+     * Returns the isModule boolean value
+     * @return The isModule
+     */
+    @JsonIgnore
+    public boolean isModule( ) 
+    {
+        return _bIsModule;
+    }
+
+    /**
+     * Sets the isModule flag
+     * @param _bIsModule The isModule boolean value
+     */
+    @JsonIgnore
+    public void setIsModule( boolean _bIsModule ) 
+    {
+        this._bIsModule = _bIsModule;
+    }
+    
 }

src/java/fr/paris/lutece/plugins/pluginwizard/service/MapperService.java
@@ -33,6 +33,7 @@
  */
 package fr.paris.lutece.plugins.pluginwizard.service;
 
+import org.codehaus.jackson.map.DeserializationConfig;
 import fr.paris.lutece.plugins.pluginwizard.business.model.PluginModel;
 import fr.paris.lutece.portal.service.util.AppLogService;
 

src/java/fr/paris/lutece/plugins/pluginwizard/service/generator/AbstractGenerator.java
@@ -82,13 +82,13 @@ public abstract class AbstractGenerator implements Generator
     {
         String strBasePath = "plugin-{plugin_name}/" + strPath;
         
-        if(Utils.MODULE.equals(pm.getType())){
-        	
-        	strBasePath = "module-"+pm.getPluginName(  )+"/"+ strPath.replace( "{plugin_name}", (pm.getPluginName(  ).split("-")[0]  + "/modules/" +pm.getPluginName(  ).split("-")[1]));
-        	
-        }else{
-        	
-        	strBasePath = strBasePath.replace( "{plugin_name}", pm.getPluginName(  ) );
+        if ( pm.isModule( ) ) {
+            strBasePath = "module-" + pm.getPluginName(  ) + "/" 
+                    + strPath.replace( "{plugin_name}", (pm.getPluginName(  ).split("-")[0]  + "/modules/" +pm.getPluginName(  ).split("-")[1]));
+        }
+        else
+        {
+            strBasePath = strBasePath.replace( "{plugin_name}", pm.getPluginName(  ) );
         }
         
         

src/java/fr/paris/lutece/plugins/pluginwizard/service/generator/AdminJspControllerGenerator.java
@@ -78,15 +78,9 @@ public class AdminJspControllerGenerator extends AbstractGenerator
     public Map generate( PluginModel pm )
     {
         HashMap map = new HashMap(  );
-        boolean bIsModule;
-        String strPluginName = pm.getPluginName(  );
-        if(Utils.MODULE.equals(pm.getType( ))){
-        	strPluginName= pm.getPluginName( ).split("-")[0]+".modules."+pm.getPluginName( ).split("-")[1];
-        	bIsModule= true;
-        }else{
-        	strPluginName= pm.getPluginName( );
-        	bIsModule= false;
-        }
+        
+        String strPluginName = pm.getPluginNameAsRadicalPackage( );
+        
         for ( Feature feature : pm.getFeatures(  ) )
         {
             List<BusinessClass> listBusinessClasses = ModelService.getBusinessClassesByFeature( pm, feature.getId(  ) );
@@ -97,7 +91,7 @@ public class AdminJspControllerGenerator extends AbstractGenerator
 
                 String strPath = getFilePath( pm, PATH, strJspFileName );
 
-                String strSourceCode = getJspBusinessFile( businessClass, feature.getFeatureName(  ), strPluginName, bIsModule );
+                String strSourceCode = getJspBusinessFile( businessClass, feature.getFeatureName(  ), strPluginName, pm.isModule( ) );
                 strSourceCode = strSourceCode.replace( "&lt;", "<" );
                 strSourceCode = strSourceCode.replace( "&gt;", ">" );
                 map.put( strPath, strSourceCode );

src/java/fr/paris/lutece/plugins/pluginwizard/service/generator/AdminJspGenerator.java
@@ -82,19 +82,8 @@ public class AdminJspGenerator extends AbstractGenerator
     public Map generate( PluginModel pm )
     {
         HashMap map = new HashMap(  );
-        String strPluginName = pm.getPluginName(  );
-        boolean bIsModule= false;
+        String strPluginName = pm.getPluginNameAsRadicalPackage();
         
-        if(Utils.MODULE.equals(pm.getType( ))){
-        	
-        	bIsModule=true;
-        	strPluginName= pm.getPluginName( ).split("-")[0]+".modules."+pm.getPluginName( ).split("-")[1];
-        	
-        }else{
-        	
-        	strPluginName= pm.getPluginName( );
-        }
-
         for ( Feature feature : pm.getFeatures(  ) )
         {
             List<BusinessClass> listBusinessClasses = ModelService.getBusinessClassesByFeature( pm, feature.getId(  ) );
@@ -109,7 +98,7 @@ public class AdminJspGenerator extends AbstractGenerator
                     String strPath = getFilePath( pm, PATH, strJspFileName );
 
                     String strSourceCode = getJspBusinessFile( businessClass, feature.getFeatureName(  ),
-                            strPluginName, i + 1, bIsModule );
+                            strPluginName, i + 1, pm.isModule() );
                     strSourceCode = strSourceCode.replace( "&lt;", "<" );
                     strSourceCode = strSourceCode.replace( "&gt;", ">" );
                     map.put( strPath, strSourceCode );
@@ -118,7 +107,7 @@ public class AdminJspGenerator extends AbstractGenerator
 
             String strPath = getFilePath( pm, PATH, feature.getFeatureName(  ) + EXT_JSP );
 
-            String strSourceCode = getFeatureJspFile( feature.getFeatureName(  ), strPluginName, bIsModule );
+            String strSourceCode = getFeatureJspFile( feature.getFeatureName(  ), strPluginName, pm.isModule() );
             strSourceCode = strSourceCode.replace( "&lt;", "<" );
             strSourceCode = strSourceCode.replace( "&gt;", ">" );
             map.put( strPath, strSourceCode );

src/java/fr/paris/lutece/plugins/pluginwizard/service/generator/AdminTemplateGenerator.java
@@ -66,17 +66,11 @@ public class AdminTemplateGenerator extends AbstractGenerator
     public Map generate( PluginModel pm )
     {
         HashMap map = new HashMap(  );
-        String strPluginName, strLink;
-        if(Utils.MODULE.equals(pm.getType( ))){
-        	
-        	strPluginName= "module."+pm.getPluginName( ).replace("-", ".");
-        	strLink= pm.getPluginName( ).split("-")[0]+"/modules/"+pm.getPluginName( ).split("-")[1];
-        	
-        }else{
-        	
-        	strPluginName= pm.getPluginName( );
-        	strLink= pm.getPluginName( );
-        }
+        
+        String strPluginName = strPluginName = pm.getPluginNameAsPackage() ;
+        String strLink = pm.getPluginNameAsRadicalPath() ;
+        
+
 
         //for each feature,which business classes are attached to
         Collection<Feature> listFeatures = pm.getFeatures(  );

src/java/fr/paris/lutece/plugins/pluginwizard/service/generator/BusinessClassGenerator.java
@@ -70,17 +70,11 @@ public class BusinessClassGenerator extends AbstractGenerator
     {
         HashMap<String, String> map = new HashMap<String, String>(  );
         Collection<BusinessClass> listAllBusinessClasses = pm.getBusinessClasses(  );
-        String strPluginName, strRadicalPackage;
-        if(Utils.MODULE.equals(pm.getType( ))){
-        	
-        	strPluginName= "module."+pm.getPluginName( ).replace("-", ".");
-        	strRadicalPackage= pm.getPluginName( ).split("-")[0]+".modules."+pm.getPluginName( ).split("-")[1];
-        	
-        }else{
-        	
-        	strPluginName= pm.getPluginName( );
-        	strRadicalPackage= pm.getPluginName( );
-        }
+        
+        String strPluginName = pm.getPluginNameAsPackage( ) ;
+        String strRadicalPackage = pm.getPluginNameAsRadicalPackage( ) ;
+        
+        
         for ( BusinessClass businessClass : listAllBusinessClasses )
         {
             for ( BusinessFileConfig file : _listFiles )

commit c57c1dbde1fbbc6514bd02e4e10a8d2939fe8b95

PLUGINWIZARD-114 : fix resource name of module projects (regression from 1c2ee9c87b9)

src/java/fr/paris/lutece/plugins/pluginwizard/service/generator/ResourcesGenerator.java
@@ -73,7 +73,7 @@ public class ResourcesGenerator extends AbstractGenerator
         for ( String strLanguage : _languages )
         {
             String strPath = getFilePath( pm, PATH,
-                    pm.getPluginName(  ).toLowerCase(  ) + "_messages" 
+                    prefixFileName + "_messages" 
                                 + (strLanguage.length()>0?"_":"") 
                                 + strLanguage + ".properties" );
 